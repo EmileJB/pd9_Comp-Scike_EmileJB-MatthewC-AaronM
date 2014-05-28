535aa772beaa07aeb2cd63e4df1741ec468dcf39
Matthew <mcookhome@gmail.com>
2014-05-28 14:46:58 -0400

    changed devlog

--- Board.java
+++ Board.java
@@ -1,11 +1,22 @@
 import info.gridworld.grid.*;
-
+import info.gridworld.actor.*;
+import info.gridworld.actor.ActorWorld;
+import java.awt.Color;
 
 public class Board {
     public static void main (String[] args) {
-	BoundedGrid<Loc> b = new BoundedGrid<Loc> ( 10,10);
+	ActorWorld w = new ActorWorld();
+	w.add(new Bug());
+	Flower j = new Flower();
+	w.add(new Location(2,2), j);
+	j.setDirection(Location.EAST);
+	
+	w.show();
+	/*
+	BoundedGrid<Location> b = new BoundedGrid<Location> ( 10,10);
 	Game g = new Game( b);
        	System.setProperty("info.gridworld.gui.selection", "hide");
 	g.show();
+	*/
     }
 }
--- Game.java
+++ Game.java
@@ -1,11 +1,11 @@
 import info.gridworld.grid.*;
 import info.gridworld.world.*;
 
-public class Game extends World<Loc> {
-    private Loc[] [] crib = new Loc[10][10];
+public class Game extends World<Location> {
+    private Location[] [] crib = new Location[10][10];
     private boolean placeable;
 
-    public Game(BoundedGrid<Loc> b){
+    public Game(BoundedGrid<Location> b){
 	super(b);
 	start();
     }
@@ -13,8 +13,7 @@ public class Game extends World<Loc> {
     public void start() {
 	for (int r =0; r<=9; r++){
 	    for (int c=0; c <=9; c++){
-		crib [r][c]=new Loc();
-		add(new Location(r, c), crib[r][c]);
+		crib[r][c]= new Location(r, c);
 	    }
 	}
 	

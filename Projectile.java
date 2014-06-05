1e096985dc4446bddc2daffb969b78abd402ddd1
Matthew Cook <mcookhome@gmail.com>
2014-06-05 18:32:27 -0400

    projectiles beautiful :*

--- Projectile.java
+++ Projectile.java
@@ -10,30 +10,42 @@ public class Projectile extends Actor {
     protected int id;
     protected Loc  target;
     protected Enemy mark;
-    protected Image img ,nw;
+    protected Image img ,nw,ne,n,s,e,w,se,sw;
     protected int damage;
     protected int range;
     protected int speed,basespeed;
     protected Grid g;
     protected Gui gu;
+    protected boolean acted;
     
-    public Projectile(Loc spawn, Enemy e, Gui gu, int d, int r, int s){
+    public Projectile(Loc spawn, Enemy en, Gui gu, int d, int r, int sp){
 	super(spawn);
 	damage = d;
 	id=3;
 	//location=location;
-	mark = e;
+	mark = en;
 	target = mark.getLoc();	
 	g= location.getGrid();
 	gu = g.getGui();
 	range = r;
-	basespeed = speed = s;
+	basespeed = speed = sp;
+	acted = false;
 	//System.out.println(gu);
 	try {
-	    img = ImageIO.read(new File ( "images/misnw.gif"));
+	    
+	    nw = ImageIO.read(new File ( "images/misnw.gif"));
+	    ne = ImageIO.read(new File ( "images/misne.gif"));
+	    w = ImageIO.read(new File ( "images/misw.gif"));
+	    n = ImageIO.read(new File ( "images/misn.gif"));
+	    s = ImageIO.read(new File ( "images/miss.gif"));
+	    e = ImageIO.read(new File ( "images/mise.gif"));
+	    se = ImageIO.read(new File ( "images/misse.gif"));
+	    sw = ImageIO.read(new File ( "images/missw.gif"));
+
 	}catch (IOException ex) {
 	    System.out.println("you goofed Dx");
 	}
+	img = nw;
 	location.addActor(this);
 	gu.addProjectile(this);
     }
@@ -44,7 +56,7 @@ public class Projectile extends Actor {
     public void moveOne(int x, int y) {
 	//gu = g.getGui();
 	//Loc tmp = location;
-	location.removeActor(this);
+	//location.removeActor(this);
 	g.getLoc(location.getX(),location.getY()).removeActor(this);
 	//System.out.println(location.getActors());
 	//g.placeActor(x,y, this);
@@ -61,46 +73,53 @@ public class Projectile extends Actor {
 	    if (location.getX() > target.getX() && location.getY() > target.getY()){
 		//System.out.println("1");
 		moveOne(location.getX() -1, location.getY() - 1);
+		setImg(nw);
 	    }
 	    else if (location.getX()< target.getX() && location.getY() > target.getY()){
 		//System.out.println("2");
 		moveOne(location.getX() +1, location.getY() - 1);
+		setImg(sw);
 	    }
 	    else if (location.getX()< target.getX() && location.getY() < target.getY()){
 		//	System.out.println("3");
 		moveOne(location.getX() +1, location.getY() + 1);
+		setImg(se);
 	    }
 	    else if (location.getX()> target.getX() && location.getY() < target.getY()){
 		//	System.out.println("4");
 		moveOne(location.getX() -1, location.getY() + 1);
+		setImg(ne);
 	    }
 	    else if (location.getX()==target.getX() && location.getY() < target.getY()){
 		//		System.out.println("5");
 		moveOne(location.getX(), location.getY() + 1);
+		setImg(e);
 	    }
 	    else if (location.getX()==target.getX() && location.getY() > target.getY()){
 		//System.out.println("6");		
 		moveOne(location.getX(), location.getY() - 1);
+		setImg(w);
 	    }
 	    else if (location.getX() <target.getX() && location.getY()== target.getY()){
 		//System.out.println("7");		
 		moveOne(location.getX()+1, location.getY());
+		setImg(s);
 	    }
 	    else if(location.getX() >target.getX() && location.getY() == target.getY()){
 		    
 		//		System.out.println("8");
 		moveOne(location.getX()-1, location.getY());
+		setImg(n);
 	    }
 	    speed = basespeed;
 	    range--;
 	}
 	else
 	    speed--;
-	System.out.println(location.getActors());
+	//System.out.println(location.getActors());
     }
-
     public void checkPos() {
-	System.out.println("check2099: " + location.getEnemy());
+	//System.out.println("check2099: " + location.getEnemy());
 	
 	if (location.getEnemy() != null) {
 	    location.getEnemy().damage(damage);
@@ -131,12 +150,18 @@ public class Projectile extends Actor {
     }
 
     public void act() {
-	checkEnemy();
-	moveDirection();
-	checkPos();
-	if (mark != null)
-	    setTarget(mark.getLoc());
-    }                         
+	if (acted==false){
+	  
+	    acted=true;
+	}
+	else{
+	    checkEnemy();
+	    moveDirection();
+	    checkPos();
+	    if (mark != null)
+		setTarget(mark.getLoc());                     
+	}   
+    }
      	
 	//	while (!(location.getX()== target.getX() && location.getY() == target.getY())) {
 	//System.out.println("X: "+target.getX()+location.getX() + "\nY: "+ target.getY() + location.getY()
@@ -151,7 +176,9 @@ public class Projectile extends Actor {
     public Image getImg(){
 	return img;
     }
-
+    public void setImg(Image i){
+	img = i;
+    }
     public int ID(){
 	return id;
     }
